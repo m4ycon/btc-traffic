2504e6ab8ade5953aa9e3f15da5dcc5f45e07231
Maycon Fabio <m.vinnicyus@gmail.com>
2025-09-16 22:55:38 -0300

    Implement bad-txnmrklroot and bad-witness-nonce-size mutated blocks

--- src/utils/mutated_block.rs
+++ src/utils/mutated_block.rs
@@ -1,50 +1,21 @@
 use bitcoin::blockdata::block::Block;
+use bitcoin::hashes::Hash;
+use bitcoin::opcodes::OP_TRUE;
+use bitcoin::transaction::Version;
+use bitcoin::{Amount, Sequence};
 use bitcoin::{opcodes::all::OP_RETURN, script::Builder, Address};
 use corepc_node::{Client, Error};
 
 use crate::utils::create_block::{mine_block, update_merkle_root};
 use crate::utils::{create_block::create_block, create_transaction::create_transaction};
 
-// bad-txnmrklroot
 pub async fn create_mutated_block_1(client: &Client, to_address: &Address) -> Result<Block, Error> {
-    let block = create_mutated_block(client, to_address, |block| {
-        todo!()
-    })
-    .await
-    .unwrap();
-
-    Ok(block)
-}
-
-// bad-txns-duplicate
-pub async fn create_mutated_block_2(client: &Client, to_address: &Address) -> Result<Block, Error> {
-    let block = create_mutated_block(client, to_address, |block| {
-        todo!()
-    })
-    .await
-    .unwrap();
-
-    Ok(block)
-}
-
-// bad-witness-nonce-size
-pub async fn create_mutated_block_3(client: &Client, to_address: &Address) -> Result<Block, Error> {
-    let block = create_mutated_block(client, to_address, |block| {
-        todo!()
-    })
-    .await
-    .unwrap();
-
-    Ok(block)
-}
-
-// bad-witness-merkle-match
-pub async fn create_mutated_block_4(client: &Client, to_address: &Address) -> Result<Block, Error> {
-    let block = create_mutated_block(client, to_address, |block| {
-        // change commitment in coinbase to invalid value
-        let coinbase = block.txdata.first_mut().unwrap();
-        let script_pubkey_bytes = coinbase.output[0].script_pubkey.as_mut_bytes();
-        script_pubkey_bytes[script_pubkey_bytes.len() - 1] ^= 0x01;
+    // just logged by btc-core in debug=validation mode
+    let block = create_mutated_block(client, to_address, "bad-txnmrklroot", |block| {
+        let merkle_root = block.header.merkle_root.clone();
+        let mut bytes = *merkle_root.as_raw_hash().as_byte_array();
+        bytes[0] ^= 0x55;
+        block.header.merkle_root = Hash::from_byte_array(bytes);
         Ok(())
     })
     .await
@@ -53,12 +24,49 @@ pub async fn create_mutated_block_4(client: &Client, to_address: &Address) -> Re
     Ok(block)
 }
 
-// unexpected-witness
+pub async fn create_mutated_block_2(client: &Client, to_address: &Address) -> Result<Block, Error> {
+    let block = create_mutated_block(client, to_address, "bad-txns-duplicate", |block| todo!())
+        .await
+        .unwrap();
+
+    Ok(block)
+}
+
+pub async fn create_mutated_block_3(client: &Client, to_address: &Address) -> Result<Block, Error> {
+    let block = create_mutated_block(client, to_address, "bad-witness-nonce-size", |block| {
+        // add extra item to witness stack
+        let coinbase = block.txdata.first_mut().unwrap();
+        coinbase.input[0].witness.push([0]);
+        update_merkle_root(block);
+        Ok(())
+    })
+    .await
+    .unwrap();
+
+    Ok(block)
+}
+
+pub async fn create_mutated_block_4(client: &Client, to_address: &Address) -> Result<Block, Error> {
+    let block = create_mutated_block(client, to_address, "bad-witness-merkle-match", |block| {
+        // change commitment in coinbase to invalid value
+        let coinbase = block.txdata.first_mut().unwrap();
+        let script_pubkey_bytes = coinbase.output[0].script_pubkey.as_mut_bytes();
+        script_pubkey_bytes[script_pubkey_bytes.len() - 1] ^= 0x01;
+        update_merkle_root(block);
+        Ok(())
+    })
+    .await
+    .unwrap();
+
+    Ok(block)
+}
+
 pub async fn create_mutated_block_5(client: &Client, to_address: &Address) -> Result<Block, Error> {
-    let block = create_mutated_block(client, to_address, |block| {
+    let block = create_mutated_block(client, to_address, "unexpected-witness", |block| {
         // overwrite coinbase scriptPubKey to OP_RETURN, removing witness commitment
         let coinbase = block.txdata.first_mut().unwrap();
         coinbase.output[0].script_pubkey = Builder::new().push_opcode(OP_RETURN).into_script();
+        update_merkle_root(block);
         Ok(())
     })
     .await
@@ -70,6 +78,7 @@ pub async fn create_mutated_block_5(client: &Client, to_address: &Address) -> Re
 async fn create_mutated_block(
     client: &Client,
     to_address: &Address,
+    mutate_message: &str,
     mutate_callback: fn(&mut Block) -> Result<(), Error>,
 ) -> Result<Block, Error> {
     let self_transfer = create_transaction(client, to_address).await.unwrap();
@@ -78,15 +87,10 @@ async fn create_mutated_block(
 
     mutate_callback(&mut block)?;
 
-    finalize_block(&mut block);
+    mine_block(&mut block);
 
     let block_hash = block.block_hash();
-    println!("Mutated block hash: {}", block_hash);
+    println!("Mutated block hash ({}): {}", mutate_message, block_hash);
 
     Ok(block)
-}
-
-fn finalize_block(block: &mut Block) {
-    update_merkle_root(block);
-    mine_block(block);
 }
